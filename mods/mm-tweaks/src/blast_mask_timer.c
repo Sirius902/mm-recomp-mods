a9dd368650fadc6d0c764b0d6e36769e886f1f8a
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-06-08 20:48:44 -0700

    chore: formatting

--- mods/mm-tweaks/src/blast_mask_timer.c
+++ mods/mm-tweaks/src/blast_mask_timer.c
@@ -3,8 +3,7 @@
 #include "recomputils.h"
 #include "recompconfig.h"
 
-RECOMP_HOOK("Player_ProcessItemButtons")
-void process_item_buttons_hook(Player* this, PlayState* play) {
+RECOMP_HOOK("Player_ProcessItemButtons") void process_item_buttons_hook(Player* this, PlayState* play) {
     if (recomp_get_config_u32("instant_blast_mask")) {
         this->blastMaskTimer = 0;
     }
--- mods/mm-tweaks/src/instant_putaway.c
+++ mods/mm-tweaks/src/instant_putaway.c
@@ -3,8 +3,7 @@
 #include "recomputils.h"
 #include "recompconfig.h"
 
-RECOMP_HOOK("Player_UpdateInterface")
-void update_interface_hook(PlayState* play, Player* this) {
+RECOMP_HOOK("Player_UpdateInterface") void update_interface_hook(PlayState* play, Player* this) {
     if (recomp_get_config_u32("instant_putaway")) {
         this->putAwayCooldownTimer = 0;
     }
