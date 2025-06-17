de2ca56db7d2c641eb1d637568852f7dbfe101e5
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-06-17 02:14:15 -0700

    chore: add FUTUREs

--- mods/mm-tweaks/src/blast_mask_timer.c
+++ mods/mm-tweaks/src/blast_mask_timer.c
@@ -4,6 +4,7 @@
 #include "recompconfig.h"
 
 RECOMP_HOOK("Player_ProcessItemButtons") void process_item_buttons_hook(Player* this, PlayState* play) {
+    // FUTURE(Sirius902) This implementation isn't ideal since it will still have 1 frame of delay.
     if (recomp_get_config_u32("instant_blast_mask")) {
         this->blastMaskTimer = 0;
     }
--- mods/mm-tweaks/src/instant_putaway.c
+++ mods/mm-tweaks/src/instant_putaway.c
@@ -4,6 +4,7 @@
 #include "recompconfig.h"
 
 RECOMP_HOOK("Player_UpdateInterface") void update_interface_hook(PlayState* play, Player* this) {
+    // FUTURE(Sirius902) This implementation isn't ideal since it will still have 1 frame of delay.
     if (recomp_get_config_u32("instant_putaway")) {
         this->putAwayCooldownTimer = 0;
     }
